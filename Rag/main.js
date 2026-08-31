ce53f269303d029a4e9631ea37073bb77c510db4
git.sujalsingh@gmail.com <git.sujalsingh@gmail.com>
2026-08-31 23:14:34 +0530

    understanding rag working

---
+++ Rag/main.js
@@ -0,0 +1,63 @@
+import { PDFParse } from 'pdf-parse'
+import fs from "fs"
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { MistralAIEmbeddings } from "@langchain/mistralai";
+import dotenv from "dotenv"
+import { Pinecone } from '@pinecone-database/pinecone'
+
+dotenv.config()
+
+const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY });
+const index = pc.index('rag')
+
+
+const dataBuffer = fs.readFileSync('./story.pdf')
+
+
+const parser = new PDFParse({
+    data: dataBuffer
+})
+
+const data = await parser.getText()
+
+
+const embeddings = new MistralAIEmbeddings({
+    apiKey: process.env.MISTRAL_API_KEY,
+    model: "mistral-embed",
+});
+
+const splitter = new RecursiveCharacterTextSplitter({ chunkSize: 500, chunkOverlap: 0 })
+
+const chunks = await splitter.splitText(data.text)
+
+const docs = await Promise.all(chunks.map(async (chunk)=>{
+    const embedding = await embeddings.embedQuery(chunk)
+    return {
+        text: chunk,
+        embedding
+    }
+}))
+
+const result = await index.upsert({
+    records: docs.map((doc, i) => ({
+        id: `doc-${i}`,
+        values: doc.embedding,
+        metadata: {
+            text: doc.text
+        }
+    }))
+})
+
+const queryEmbedding = await embeddings.embedQuery("how was the internship experience?")
+
+ 
+
+// const result = await index.query({
+//     vector: queryEmbedding,
+//     topK: 2,
+//     includeMetadata: true
+// })
+
+
+console.log(JSON.stringify(result))
+// console.log(chunks.length)
