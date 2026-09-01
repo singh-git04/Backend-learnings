31bd06a72efe173a2bfec456bd1549187e411acb
git.sujalsingh@gmail.com <git.sujalsingh@gmail.com>
2026-09-01 18:49:38 +0530

    implement embeddings and model working

--- Rag/main.js
+++ Rag/main.js
@@ -11,14 +11,14 @@ const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY });
 const index = pc.index('rag')
 
 
-const dataBuffer = fs.readFileSync('./story.pdf')
+// const dataBuffer = fs.readFileSync('./story.pdf')
 
 
-const parser = new PDFParse({
-    data: dataBuffer
-})
+// const parser = new PDFParse({
+//     data: dataBuffer
+// })
 
-const data = await parser.getText()
+// const data = await parser.getText()
 
 
 const embeddings = new MistralAIEmbeddings({
@@ -26,38 +26,41 @@ const embeddings = new MistralAIEmbeddings({
     model: "mistral-embed",
 });
 
-const splitter = new RecursiveCharacterTextSplitter({ chunkSize: 500, chunkOverlap: 0 })
+// const splitter = new RecursiveCharacterTextSplitter({ chunkSize: 500, chunkOverlap: 0 })
 
-const chunks = await splitter.splitText(data.text)
+// const chunks = await splitter.splitText(data.text)
+// console.log(chunks.length)
 
-const docs = await Promise.all(chunks.map(async (chunk)=>{
-    const embedding = await embeddings.embedQuery(chunk)
-    return {
-        text: chunk,
-        embedding
-    }
-}))
 
-const result = await index.upsert({
-    records: docs.map((doc, i) => ({
-        id: `doc-${i}`,
-        values: doc.embedding,
-        metadata: {
-            text: doc.text
-        }
-    }))
-})
+// const docs  = await embeddings.embedDocuments(chunks) /* 1024 dimension */
+// const docs = await Promise.all(chunks.map(async (chunk)=>{
+//     const embedding = await embeddings.embedQuery(chunk)
+//     return {
+//         text: chunk,
+//         embedding
+//     }
+// }))
+// console.log(docs)
+
+// const result = await index.upsert({
+//     records: docs.map((doc, i) => ({
+//         id: `doc-${i}`,
+//         values: doc.embedding,
+//         metadata: {
+//             text: doc.text
+//         }
+//     }))
+// })
 
 const queryEmbedding = await embeddings.embedQuery("how was the internship experience?")
 
  
 
-// const result = await index.query({
-//     vector: queryEmbedding,
-//     topK: 2,
-//     includeMetadata: true
-// })
+const result = await index.query({
+    vector: queryEmbedding,
+    topK: 2,
+    includeMetadata: true
+})
 
 
 console.log(JSON.stringify(result))
-// console.log(chunks.length)
